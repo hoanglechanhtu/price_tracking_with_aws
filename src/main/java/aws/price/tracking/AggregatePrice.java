2a89c3564c89e715f3d48265298433b104a4e985
hoanglechanhtu <tu.hoang@onemount.com>
2021-08-30 14:50:51 +0700

    Insert into DB

--- src/main/java/aws/price/tracking/AggregatePrice.java
+++ src/main/java/aws/price/tracking/AggregatePrice.java
@@ -4,20 +4,19 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class AggregatePrice implements RequestHandler<SQSEvent, String> {
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
     @Override
     public String handleRequest(SQSEvent sqsEvent, Context context) {
         LambdaLogger logger = context.getLogger();
-        logger.log("Invoked JDBCSample.getCurrentTime");
 
-        String currentTime = "unavailable";
-        // Get time from DB server
         try {
             String url = "jdbc:mysql://price-tracking.chzc0pfh4lgm.ap-southeast-1.rds.amazonaws.com:3306/price_tracking?useSSL=false";
             String username = "admin";
@@ -25,19 +24,33 @@ public class AggregatePrice implements RequestHandler<SQSEvent, String> {
             logger.log("Trying to connect");
             Connection conn = DriverManager.getConnection(url, username, password);
             Statement stmt = conn.createStatement();
-            ResultSet resultSet = stmt.executeQuery("SELECT NOW()");
 
-            if (resultSet.next()) {
-                currentTime = resultSet.getObject(1).toString();
-            }
+            String query = "INSERT INTO price_log ("
+                    + " url,"
+                    + " process_at,"
+                    + " price VALUES ("
+                    + "?, ?, ?)";
 
-            logger.log("Successfully executed query.  Result: " + currentTime);
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            sqsEvent.getRecords().forEach(event -> {
+                String body = event.getBody();
+                JsonObject jsonObject = gson.fromJson(body, JsonObject.class);
+                try {
+                    preparedStatement.setString(1, jsonObject.get("url").getAsString());
+                    preparedStatement.setLong(2, jsonObject.get("date").getAsLong());
+                    preparedStatement.setLong(3, jsonObject.get("price").getAsLong());
+                    preparedStatement.executeUpdate();
+                    preparedStatement.clearParameters();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            });
 
+            preparedStatement.close();
         } catch (Exception e) {
             e.printStackTrace();
             logger.log("Caught exception: " + e.getMessage());
         }
-
-        return currentTime;
+        return "200 OK";
     }
 }
